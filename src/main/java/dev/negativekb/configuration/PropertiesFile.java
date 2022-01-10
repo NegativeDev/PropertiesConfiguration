93fa983df178f62d5d66eac4713b48711f8b8bc3
Negative <60104846+NegativeKB@users.noreply.github.com>
2022-01-09 20:48:33 -0330

    Fixed PropertiesFIle code

--- src/main/java/dev/negativekb/configuration/PropertiesFile.java
+++ src/main/java/dev/negativekb/configuration/PropertiesFile.java
@@ -1,39 +1,30 @@
 package dev.negativekb.configuration;
 
 import com.google.common.io.ByteStreams;
+import lombok.RequiredArgsConstructor;
 
 import java.io.*;
 
+@RequiredArgsConstructor
 @SuppressWarnings("all")
 public class PropertiesFile {
 
     private final String path;
     private final String name;
-    private final Class<?> mainClass;
-
-    public PropertiesFile(Class<?> mainClass, String path, String name) {
-        this.path = path;
-        this.name = name;
-        this.mainClass = mainClass;
-    }
-
+    private File file;
+    
     public void createFile() throws IOException {
         createFile(false);
     }
 
     public void createFile(boolean printDefaults) throws IOException {
-        File file;
-        if (path == null)
-            file = new File(name);
-        else
-            file = new File(path, name);
-
+        File file = new File(path, name);
         if (file.exists())
             return;
 
         file.createNewFile();
         if (printDefaults) {
-            try (InputStream in = mainClass.getClassLoader().getResourceAsStream(name);
+            try (InputStream in = getClass().getClassLoader().getResourceAsStream(name);
                  OutputStream out = new FileOutputStream(file)) {
                 ByteStreams.copy(in, out);
             } catch (Exception e) {
@@ -43,7 +34,7 @@ public class PropertiesFile {
     }
 
     public File asFile() {
-        return new File(path, name);
+        return file;
     }
 
 
